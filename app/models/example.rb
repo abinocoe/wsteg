dcd9cdb16675dbddaec2cbe964fd593d910b754b
Abigail Coe <abigailcoe@gmail.com>
2017-08-06 18:10:12 +0100

    indentation

--- app/models/example.rb
+++ app/models/example.rb
@@ -3,6 +3,6 @@ class Example < ApplicationRecord
     validates_attachment_content_type :pdf, :content_type => ['application/pdf', 'application/msword', 'text/plain'], :if => :pdf_attached?
 
     def pdf_attached?
-    self.pdf.file?
+        self.pdf.file?
     end
 end
