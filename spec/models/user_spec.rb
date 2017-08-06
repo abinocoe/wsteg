5c86e0ab194bd2fc04b208fe7838f72eb5cba7f6
Abigail Coe <abigailcoe@gmail.com>
2017-08-06 14:36:04 +0100

    user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+    
     it 'has a valid factory' do
         expect(FactoryGirl.build(:user).save).to be true
     end
+
+    it 'is invalid without an email' do
+        expect(FactoryGirl.build(:user, email: nil).save).to be false
+    end
+
+    it 'is invalid without a unique email' do
+        user = FactoryGirl.create(:user)
+
+        expect(FactoryGirl.build(:user, email: 'example@example.com').save).to be false
+    end
+
+    it 'is invalid without a password' do
+        expect(FactoryGirl.build(:user, password: nil).save).to be false
+    end
+
 end
