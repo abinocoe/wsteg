aa96a86d64b315e8cff74dcd767dbb344e645967
Abigail Coe <abigailcoe@gmail.com>
2017-08-06 18:09:36 +0100

    fix paperclip generator bug

--- db/migrate/20170806165650_add_attachment_pdf_to_examples.rb
+++ db/migrate/20170806165650_add_attachment_pdf_to_examples.rb
@@ -1,4 +1,4 @@
-class AddAttachmentPdfToExamples < ActiveRecord::Migration
+class AddAttachmentPdfToExamples < ActiveRecord::Migration[5.1]
   def self.up
     change_table :examples do |t|
       t.attachment :pdf
