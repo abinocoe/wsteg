c1d16cdece0900f4b1ed2bcaf99ab4f64ff2e921
Abigail Coe <abigailcoe@gmail.com>
2017-08-06 15:34:50 +0100

    page model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805194539) do
+ActiveRecord::Schema.define(version: 20170806144407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string "data_file_name", null: false
+    t.string "data_content_type"
+    t.integer "data_file_size"
+    t.string "data_fingerprint"
+    t.string "type", limit: 30
+    t.integer "width"
+    t.integer "height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "indexpertise", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ spec/models/page_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Page, type: :model do
+  
+  it 'has a valid factory' do
+    expect(FactoryGirl.build(:user).save).to be true
+  end
+
+  it 'sets indexperience to false by default' do
+    page = FactoryGirl.create(:page)
+
+    expect(page.indexpertise).to be false
+  end
+
+  it 'is invalid without a title' do
+    expect(FactoryGirl.build(:page, title: nil).save).to be false
+  end
+
+  it 'is invalid without a body' do 
+    expect(FactoryGirl.build(:page, body: nil).save).to be false
+  end
+
+end
